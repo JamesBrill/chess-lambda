7dc0210dff243b3fe67afd9efb3912f79800f705
James Brill <contact@jamesbrill.co.uk>
2017-05-29 18:25:19 +0100

    Stockfish binary depends on whether run locally or not

--- handler.py
+++ handler.py
@@ -13,10 +13,10 @@ import chess.uci
 
 def endpoint(event, context):
     position = "1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1"
-    if os.environ['ENV'] == "PROD":
-        engine_binary = "./stockfish_linux"
-    else:
+    if os.environ.get('IS_LOCAL') == "true":
         engine_binary = "./stockfish_mac"
+    else:
+        engine_binary = "./stockfish_linux"
     engine = chess.uci.popen_engine(engine_binary)
     engine.uci()
     board = chess.Board(position)
