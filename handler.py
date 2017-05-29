5138b1dc8e63f42a2269f869896de4632fb10c39
James Brill <contact@jamesbrill.co.uk>
2017-05-29 17:44:23 +0100

    Add hard-coded Stockfish chess engine response

--- handler.py
+++ handler.py
@@ -1,11 +1,27 @@
+import os
+import sys
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "vendored"))
+
+
 import json
 import datetime
+import chess
+import chess.uci
 
 
 def endpoint(event, context):
-    current_time = datetime.datetime.now().time()
+    position = "1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1"
+    engine = chess.uci.popen_engine("./stockfish_linux")
+    engine.uci()
+    board = chess.Board(position)
+    engine.position(board)
+    bestmove, ponder = engine.go(movetime=2000)
+
     body = {
-        "message": "Hello, the current time is " + str(current_time)
+        "bestmove": str(bestmove),
+        "ponder": str(ponder)
     }
 
     response = {
